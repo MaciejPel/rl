bec1971bfd352c5c71328be71e488dec6c1fb7bd
MaciejPel <maciejpelczynski0@gmail.com>
2023-10-06 22:11:56 +0200

    full ssr test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import type { HandleServerError } from "@sveltejs/kit";
 export const handleError: HandleServerError = ({ error, event }) => {
 	const err = error as { message: string };
 	const is404 = err.message.includes("Not found");
+	console.log(err);
 
 	return {
 		message: is404 ? "Page you are looking for does not exist" : "Unknown error",
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,14 @@
 	import { page } from "$app/stores";
 	import { onMount } from "svelte";
 	import { themeChange } from "theme-change";
+	import { browser } from "$app/environment";
 
 	const routes = [
 		{ path: "/set", name: "Sets" },
 		{ path: "/about", name: "About" }
 	];
 
-	let isMounted = false;
-	let isDark =
-		document.documentElement.dataset.theme === "dark" ||
-		(!document.documentElement.dataset.theme &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches);
+	let isDark: boolean | null = null;
 	let currentRoute = "/";
 
 	$: currentRoute = $page.url.pathname;
@@ -21,9 +18,10 @@
 	onMount(() => {
 		document.querySelector("body")!.style.backgroundColor = "";
 		themeChange(false);
-		// setTimeout(() => {
-		isMounted = true;
-		// }, 0);
+		isDark =
+			document.documentElement.dataset.theme === "dark" ||
+			(!document.documentElement.dataset.theme &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches);
 	});
 </script>
 
@@ -42,11 +40,7 @@
 					{route.name}
 				</a>
 			{/each}
-			<a
-				href="/set/create"
-				class="hover:bg-black hover:bg-opacity-50 rounded-full p-1"
-				class:hidden={!isMounted}
-			>
+			<a href="/set/create" class="hover:bg-black hover:bg-opacity-50 rounded-full p-1">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
@@ -60,16 +54,12 @@
 			</a>
 			<button
 				class="hover:bg-black hover:bg-opacity-50 rounded-full p-1 fill-white"
+				class:hidden={isDark === null}
 				data-toggle-theme="light,dark"
 				on:click={() => (isDark = !isDark)}
 			>
 				{#if isDark}
-					<svg
-						class="w-6 h-6"
-						class:hidden={!isMounted}
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-					>
+					<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path
 							fill-rule="evenodd"
 							d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
@@ -77,12 +67,7 @@
 						/>
 					</svg>
 				{:else}
-					<svg
-						class="w-6 h-6"
-						class:hidden={!isMounted}
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-					>
+					<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path
 							d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
 						/>
