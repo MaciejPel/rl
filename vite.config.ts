fa34a365e471adccaa1154d1661b0fdc84322251
MaciejPel <maciejpelczynski0@gmail.com>
2023-10-14 22:40:41 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		port: 3000,
 		hmr: {
 			protocol: "ws",
 			host: "localhost"
